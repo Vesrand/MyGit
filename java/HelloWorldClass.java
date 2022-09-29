60474fffcf1411fa94197054784715a7850ff083
Vesrand <35408753+Vesrand@users.noreply.github.com>
2022-09-30 00:28:49 +0300

    Revert "java branch"

--- java/HelloWorldClass.java
+++
@@ -1,8 +0,0 @@
-class HelloWorldClass {
-	public static void main (String[] args){
-		VoiceText changingTextHere;
-		changingTextHere = new VoiceText ("say this");
-		System.out.println("Hello World");
-		changingTextHere.soundOut();
-	}
-}
--- java/VoiceText.java
+++
@@ -1,9 +0,0 @@
-public class VoiceText {
-private String outcomingText;
-	public VoiceText (String incomingText){
-		outcomingText = incomingText;
-	}
-	public void soundOut(){
-		System.out.println(outcomingText);
-	}
-}
